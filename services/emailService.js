c6cd7b6cd105184ef683e14708fe32eb3469e1fb
Pranayraj016 <pranayraj016@gmail.com>
2025-10-27 22:18:31 +0530

    changes

--- services/emailService.js
+++ services/emailService.js
@@ -3,10 +3,13 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: true, // 465 port ke liye true
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // Render ki issues me needed
   },
 });
 
